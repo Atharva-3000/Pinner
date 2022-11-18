bd7d7523cd6c09f887ffb7f7e8a48943d127742e
Atharva_404 <deshmukhsambhajirao9@gmail.com>
2022-11-18 11:15:09 +0530

    First push

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Render} from 'react-dom';
+import  ReactDOM  from "react-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <Router>
+        <App/> 
+    </Router>,
+    document.getElementById('root')
+    );
